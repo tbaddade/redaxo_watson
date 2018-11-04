68a0738a313e9ee2cf5114d294b269882e115568
Markus Staab <maggus.staab@googlemail.com>
2018-11-04 14:16:03 +0100

    Boot vereinfacht

--- boot.php
+++ boot.php
@@ -35,19 +35,11 @@ if (rex::isBackend() && rex::getUser()) {
         }
     }
 
-    $stylesheets = $this->getProperty('stylesheets');
-
-    if (count($stylesheets)) {
-        foreach ($stylesheets as $stylesheet) {
-            rex_view::addCssFile($this->getAssetsUrl($stylesheet));
-        }
+    foreach ($this->getProperty('stylesheets', []) as $stylesheet) {
+        rex_view::addCssFile($this->getAssetsUrl($stylesheet));
     }
 
-    $javascripts = $this->getProperty('javascripts');
-
-    if (count($javascripts)) {
-        foreach ($javascripts as $javascript) {
-            rex_view::addJsFile($this->getAssetsUrl($javascript));
-        }
+    foreach ($this->getProperty('javascripts', []) as $javascript) {
+        rex_view::addJsFile($this->getAssetsUrl($javascript));
     }
 }
